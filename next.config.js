2bba823718edb866cfb25bb105f1fc2b384bd22c
xiaoxioa2 <lu.xiaolin@icloud.com>
2025-10-27 18:02:36 -0500

    Create essential files for GitHub upload

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: '',
+  assetPrefix: '',
+  images: {
+    unoptimized: true,
+    domains: ['images.pexels.com'],
+    loader: 'default'
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
