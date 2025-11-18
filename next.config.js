e74440a526876e3dbc8702a9bc4ea6df3398f560
xiaoxioa2 <lu.xiaolin@icloud.com>
2025-11-18 20:39:17 +0800

    Fix Vercel deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- // output: 'export',
-  trailingSlash: true,
-  basePath: '',
-  assetPrefix: '',
   images: {
-    unoptimized: true,
     domains: ['images.pexels.com'],
-    loader: 'default'
   },
   eslint: {
     ignoreDuringBuilds: true,
